6b29c2d29f8b712100730635ee490d873d18acb1
AnthonySamAspiRamos <aaspir@fcpn.edu.bo>
2025-11-22 21:57:19 -0400

    Pruebaconeccion

---
+++ src/proyectoprogra3/Persona.java
@@ -0,0 +1,15 @@
+package proyectoprogra3;
+
+public class Persona {
+    private int ci, edad;
+    private String nombre, paterno, materno, sexo, contacto;
+    public Persona(int c, String nom, String pat,String mat, int ed, String sx, int contacto){
+        ci=c;
+        nombre=nom;
+        paterno=pat;
+        materno=mat;
+        edad=ed;
+        sexo=sx;
+    }
+    
+}
---
+++ src/proyectoprogra3/ProyectoProgra3.java
@@ -0,0 +1,9 @@
+package proyectoprogra3;
+
+public class ProyectoProgra3 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
